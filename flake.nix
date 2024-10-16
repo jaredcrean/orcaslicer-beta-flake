fac05dcecb58eb272000a457d65d5df2497e86a4
100001 <github.ixda@8shield.net>
2024-10-16 13:58:39 -0600

    trying to get video to work

--- flake.nix
+++ flake.nix
@@ -8,80 +8,77 @@
 
   outputs = { self, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      system = "x86_64-linux";
-#      pkgs = import nixpkgs { inherit system; };
-      # Import nixpkgs with allowUnfree = true
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true; 
+	};
 
-    in
-    rec {
-      packages.${system}.orcaslicer = pkgs.appimageTools.wrapType2 {
-        inherit (pkgs.gst_all_1) gstreamer gst-plugins-base gst-plugins-bad gst-plugins-good gst-libav gst-plugins-ugly;
-        name = "orca-slicer-beta";
-        src = pkgs.fetchurl {
-          url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v2.2.0-rc/OrcaSlicer_Linux_V2.2.0-rc.AppImage";
-          sha256 = "sha256-cZbX2SVznoYqap6EW4SEGzGbp4ZUAytZMtN6juqW6fw="; # Replace with actual hash
-        };
+      in rec {
+        packages.${system}.orcaslicer = pkgs.appimageTools.wrapType2 {
+          inherit (pkgs.gst_all_1) gstreamer gst-plugins-base gst-plugins-bad gst-plugins-good gst-libav gst-plugins-ugly;
+          name = "orca-slicer-beta";
+          src = pkgs.fetchurl {
+            url = "https://github.com/SoftFever/OrcaSlicer/releases/download/v2.2.0-rc/OrcaSlicer_Linux_V2.2.0-rc.AppImage";
+            sha256 = "sha256-cZbX2SVznoYqap6EW4SEGzGbp4ZUAytZMtN6juqW6fw="; # Replace with actual hash
+          };
 
-        extraPkgs = pkgs: with pkgs; [
-          glibc
-          zlib
-          gtk3
-          glib
-          gtkmm3
-          glibmm
-          glibcLocales
-          cairo
-          cairomm
-          gdk-pixbuf
-          glew
-          glfw
-          glib
-          gmp
-          libavif
-          gmp
-	  glib-networking
-          gst_all_1
-          gst_all_1.gstreamer
-          gst_all_1.gst-libav
-          gst_all_1.gst-plugins-base
-          gst_all_1.gst-plugins-bad
-          gst_all_1.gst-plugins-ugly
-          gst_all_1.gst-plugins-good
-          gtest
-          gtk3
-          hicolor-icon-theme
-          ilmbase
-          libpng
-          fontconfig
-          freetype
-          libpng
-          #          libxcb
-          #          libxkbcommon
-          mesa
-          dbus
-          at-spi2-atk
-          at-spi2-core
-          webkitgtk
-          cacert
-        ];
+          extraPkgs = pkgs: with pkgs; [
+            glibc
+            zlib
+            gtk3
+            glib
+            gtkmm3
+            glibmm
+            glibcLocales
+            cairo
+            cairomm
+            gdk-pixbuf
+            glew
+            glfw
+            glib
+            gmp
+            libavif
+            gmp
+            glib-networking
+            gst_all_1
+            gst_all_1.gstreamer
+            gst_all_1.gst-libav
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-bad
+            gst_all_1.gst-plugins-ugly
+            gst_all_1.gst-plugins-good
+            gtest
+            gtk3
+            hicolor-icon-theme
+            ilmbase
+            libpng
+            fontconfig
+            freetype
+            libpng
+            #          libxcb
+            #          libxkbcommon
+            mesa
+            dbus
+            at-spi2-atk
+            at-spi2-core
+            webkitgtk
+            cacert
+          ];
 
-        extraEnv = {
-          SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-          SSL_CERT_DIR = "${pkgs.cacert}/etc/ssl/certs";
-        };
+          extraEnv = {
+            SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+            SSL_CERT_DIR = "${pkgs.cacert}/etc/ssl/certs";
+          };
 
-        meta = with pkgs.lib; {
-          description = "Orca Slicer 3D printer software";
-          homepage = "https://github.com/SoftFever/OrcaSlicer";
-          license = licenses.gpl3Plus;
-          platforms = platforms.linux;
+          meta = with pkgs.lib; {
+            description = "Orca Slicer 3D printer software";
+            homepage = "https://github.com/SoftFever/OrcaSlicer";
+            license = licenses.gpl3Plus;
+            platforms = platforms.linux;
+          };
         };
       };
-    };
+   )
 }
