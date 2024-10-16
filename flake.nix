1e028cbc1d7884c32ad93fae12974d981a6508c6
100001 <github.ixda@8shield.net>
2024-10-16 14:04:45 -0600

    trying to get video to work

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         system = "x86_64-linux";
